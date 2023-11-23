7ff94c91152edea7213a38430ee5789726d3185e
sabinasmith22 <78496806+sabinasmith22@users.noreply.github.com>
2023-11-23 00:31:30 -0500

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "codespacesreact8c8c7086": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Amplify } from 'aws-amplify';
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
+import "@aws-amplify/ui-react/styles.css"
+
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
+
+function App({ signOut, user}) {
+  return (
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onCLick={signOut}>Sign out</button>
+    </>
+  );
+}
+
+export default withAuthenticator(App);
